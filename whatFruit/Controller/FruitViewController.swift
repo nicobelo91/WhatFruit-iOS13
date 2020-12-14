85d95a90e08078141dd2457f66d1dd4d9c532617
Nico Cobelo <ingcobelo@gmail.com>
2020-12-14 20:29:59 -0300

    Get classifications from model

--- whatFruit/Controller/FruitViewController.swift
+++ whatFruit/Controller/FruitViewController.swift
@@ -36,7 +36,7 @@ class FruitViewController: UIViewController, UIImagePickerControllerDelegate, UI
             guard let ciImage = CIImage(image: userPickedImage) else {
                 fatalError("Could not convert UIImage into CIImage")
             }
-            detect(flowerImage: ciImage)
+            detect(fruitImage: ciImage)
         }
         
         imagePicker.dismiss(animated: true, completion: nil)
@@ -44,8 +44,30 @@ class FruitViewController: UIViewController, UIImagePickerControllerDelegate, UI
     
     // MARK: - Detect Method
     
-    func detect(flowerImage: CIImage) {
+    func detect(fruitImage: CIImage) {
+        guard let model = try? VNCoreMLModel(for: FruitClassifier().model) else {
+            fatalError("Loading CoreML model failed")
+        }
         
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            guard let classification = request.results as?
+                    [VNClassificationObservation] else {
+                fatalError("Model failed to process image")
+            }
+            
+            if let firstResult = classification.first {
+                let fruitName = firstResult.identifier
+                self.navigationItem.title = fruitName
+            }
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: fruitImage)
+        
+        do {
+            try handler.perform([request])
+        } catch {
+            print(error)
+        }
     }
     
     // MARK: - Camera Tapped
